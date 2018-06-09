f4c62d32db56d0514161d4ca5e2884a0a45393e5
bebarb1979 <32182686+bebarb1979@users.noreply.github.com>
2018-06-09 14:23:41 -0500

    Add files via upload
    fixed wording on modal

--- faucet_info.php
+++ faucet_info.php
@@ -173,7 +173,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="filterModalLabel">Faucet Filter</h5>
+        <h5 class="modal-title" id="filterModalLabel">Delete Faucet</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
